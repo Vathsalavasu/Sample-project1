62e1c07acd09261cd3cfc93ab6c363f216930bf7
Vathsalavasu <write2vathsalar@gmail.com>
2024-10-22 16:26:32 +0530

    first project

---
+++ HelloRestApi/src/main/java/com/helloworld/api/ApiController.java
@@ -0,0 +1,15 @@
+package com.helloworld.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+	
+	@GetMapping("/api/hello")
+	public Response hello() {
+		return new Response("Hello world REST API");
+		
+	}
+
+}
